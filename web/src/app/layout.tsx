6d5628b9e95a7cdc7512bbc52567dff473dc3e5c
jasonhao518 <jasonhao518@gmail.com>
2024-02-21 00:35:23 +0100

    sdfasdf

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,92 +1,12 @@
-import "./globals.css";
+import { ReactNode } from 'react';
+import './globals.css';
 
-import { fontHeading, fontSans, fontUrban } from "@/assets/fonts";
-import { Analytics } from "@/components/analytics";
-import { ModalProvider } from "@/components/modal-provider";
-import { ThemeProvider } from "@/components/providers";
-import { TailwindIndicator } from "@/components/tailwind-indicator";
-import { Toaster } from "@/components/ui/toaster";
-import { siteConfig } from "@/config/site";
-import { cn } from "@/lib/utils";
-import Script from "next/script";
-import { ClerkProvider, auth } from '@clerk/nextjs'
+type Props = {
+  children: ReactNode;
+};
 
-interface RootLayoutProps {
-  children: React.ReactNode,
-  params: { locale: string };
-}
-
-export const metadata = {
-  title: {
-    default: siteConfig.name,
-    template: `%s | ${siteConfig.name}`,
-  },
-  description: siteConfig.description,
-  keywords: [
-    "Next.js",
-    "React",
-    "Prisma",
-    "PlanetScale",
-    "Auth.js",
-    "shadcn ui",
-    "Resend",
-    "React Email",
-    "Stripe"
-  ],
-  authors: [
-    {
-      name: "mickasmt",
-    },
-  ],
-  creator: "mickasmt",
-  metadataBase: new URL(siteConfig.url),
-  openGraph: {
-    type: "website",
-    locale: "en_US",
-    url: siteConfig.url,
-    title: siteConfig.name,
-    description: siteConfig.description,
-    siteName: siteConfig.name,
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: siteConfig.name,
-    description: siteConfig.description,
-    images: [siteConfig.ogImage],
-    creator: "@miickasmt",
-  },
-  icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon-16x16.png",
-    apple: "/apple-touch-icon.png",
-  },
-  manifest: `${siteConfig.url}/site.webmanifest`,
-}
-
-export default async function RootLayout({ children, params }: RootLayoutProps) {
-  const user = await auth()
-
-  return (
-    <ClerkProvider>
-
-      <html lang="en" suppressHydrationWarning>
-        <body
-          className={cn(
-            "min-h-screen bg-background font-sans antialiased",
-            fontSans.variable,
-            fontUrban.variable,
-            fontHeading.variable
-          )}
-        >
-          <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-            {children}
-            <Toaster />
-            <TailwindIndicator />
-          </ThemeProvider>
-          <Script id="my-script">{`window.$crisp=[];window.CRISP_WEBSITE_ID="48739dc7-7b9f-43ff-b6eb-cd9bc672ce88";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();`}</Script>
-
-        </body>
-      </html>
-    </ClerkProvider>
-  )
+// Since we have a `not-found.tsx` page on the root, a layout file
+// is required, even if it's just passing children through.
+export default function RootLayout({ children }: Props) {
+  return children;
 }
