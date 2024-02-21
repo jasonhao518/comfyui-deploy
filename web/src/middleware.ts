455f7ac7b32b1591979bc364a1fe4e16f32bcd71
jasonhao518 <jasonhao518@gmail.com>
2024-02-21 02:15:59 +0100

    Update middleware.ts

--- web/src/middleware.ts
+++ web/src/middleware.ts
@@ -15,7 +15,7 @@ export default authMiddleware({
     return I18nMiddleware(req);
   },
   // debug: true,
-  publicRoutes: ["/", "/:locale", "/:locale/pricing", "/api/(.*)", "/:locale/api/(.*)", "/docs(.*)", "/:locale/docs(.*)", "/share(.*)", "/:locale/share(.*)"],
+  publicRoutes: ["/", "/:locale", "/:locale/pricing", "/:locale/examples", "/api/(.*)", "/:locale/api/(.*)", "/docs(.*)", "/:locale/docs(.*)", "/share(.*)", "/:locale/share(.*)"],
   // publicRoutes: ["/", "/(.*)"],
   async afterAuth(auth, req, evt) {
     // redirect them to organization selection page
